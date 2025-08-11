528f2d9e05f3aad80b2efe9f790b3534a4bb8bdb
ploiju <kgpsophomores@gmail.com>
2025-08-12 02:56:59 +0530

    Check for base href to generate absolute url in _parse_html5_media_entries

--- yt_dlp/extractor/common.py
+++ yt_dlp/extractor/common.py
@@ -3341,7 +3341,11 @@ class InfoExtractor:
         return formats, subtitles
 
     def _parse_html5_media_entries(self, base_url, webpage, video_id, m3u8_id=None, m3u8_entry_protocol='m3u8_native', mpd_id=None, preference=None, quality=None, _headers=None):
+        base_href = self._search_regex(r'<base\s+href\s*=\s*["\']([^"\']+)["\']', webpage, 'base href', default=None)
+
         def absolute_url(item_url):
+            if base_href is not None:
+                return urljoin(base_href, item_url)
             return urljoin(base_url, item_url)
 
         def parse_content_type(content_type):
